0443cf3b6653f14b8259aea7ec1852aeca80f742
ivan coronado <softeam@MacBook-Pro-de-Ivan.local>
2014-05-24 02:02:33 -0500

    namespace

--- Test.php
+++ Test.php
@@ -1,9 +1,10 @@
 <?php
 
+namespace testppm;
 /**
  * Concrete class for generating debug dumps related to the output source.
  */
-class Test extends Phalcon\Mvc\User\Component
+class Test extends \Phalcon\Mvc\Application
 {
     public function test($test=null){
         return $test.'_Mod';
